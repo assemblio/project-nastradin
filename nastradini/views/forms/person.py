f8bd5f7f9ceccfd214bb28425dd7b9c35ce90b76
legeorges <georges.labreche@gmail.com>
2014-10-26 16:24:05 -0700

    Added coordinates

--- nastradini/views/forms/person.py
+++ nastradini/views/forms/person.py
@@ -8,6 +8,29 @@ class Person(MethodView):
 
     methods = ['GET', 'POST']
 
+    coordinates = {
+        "Prishtina": {
+            "lat": 42.6662068,
+            "lon": 21.1599254
+        },
+        "Ferizaj": {
+            "lat": 42.3719071,
+            "lon": 21.1511922
+        },
+        "Gjakova": {
+            "lat": 42.3911916,
+            "lon": 20.4279613
+        },
+        "Gjilan": {
+            "lat": 42.4589712,
+            "lon": 21.4661479
+        },
+        "Prizren": {
+            "lat": 42.2181194,
+            "lon": 20.7407284
+        }
+    }
+
     def get(self):
         form = PersonForm()
         return render_template('person.html', form=form)
@@ -19,6 +42,10 @@ class Person(MethodView):
         # Create patient info object.
         person_form = PersonForm(request.form)
         person_json = person_form.data
+
+        # Set coordinates
+        city = person_json['city']
+        person_json['coordinates'] = self.coordinates[city]
 
         # Store the document.
         mongo.db.persons.update({'_id': doc_id}, person_json, True)
