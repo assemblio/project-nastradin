20c4fe65505ecaced1047b39beb07afc1ab724eb
legeorges <georges.labreche@gmail.com>
2014-10-26 17:30:01 -0700

    todo comments

--- nastradini/views/forms/searchform.py
+++ nastradini/views/forms/searchform.py
@@ -8,6 +8,6 @@ class SearchForm(Form):
     industry = TextField('Industry') #TODO: This needs to be select list: [All, Construction, Education, Government, Technology]
     profession = TextField('Profession')  #TODO: This needs to be select list: [Electrician, Teacher, Project Manager, Software Engineer]
     minimum_salary = TextField(
-        'Minimum Salary (Monthly)')
+        'Minimum Salary (Monthly)') #TODO: this is list: [200, 300, 400, 500, 600, 700, 800, 900]
     maximum_salary = TextField(
-        'Maximum Salary (Monthly)')
+        'Maximum Salary (Monthly)') #TODO: this is list: [300, 400, 500, 600, 700, 800, 900, 1000+]
