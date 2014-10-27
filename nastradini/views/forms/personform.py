16babbe2c3a179b28324a5dc3937778a8293b10a
endrit-b <endriti.92@gmail.com>
2014-10-27 01:14:32 +0100

    form arrangedto required type

--- nastradini/views/forms/personform.py
+++ nastradini/views/forms/personform.py
@@ -1,17 +1,65 @@
 from flask_wtf import Form
-from wtforms import TextField, TextAreaField, RadioField, DateField
+from wtforms import TextField, TextAreaField, RadioField, DateField, SelectField
 
 
 class PersonForm(Form):
+
+    cities_list = [
+        'Prishtina', 'Ferizaj',
+        'Gjakova', 'Gjilan',
+        'Prizren'
+    ]
+
+    cities_tuple = [
+        (x, x) for x in cities_list
+    ]
 
     first_name = TextField('First Name')
     last_name = TextField('Last Name')
     birth_date = DateField('Birth Date')
     gender = RadioField('Gender', choices=[('Male', 'Male'), ('Female', 'Female')], default='Mashkull')
-    city = TextField('City') #TODO: This needs to be select list: [Prishtina, Ferizaj, Gjakova, Gjilan, Prizren]
-    highest_level_of_education = TextField('Highest Level of Education') #TODO: This needs to be select list: ["High School", "Undergraduate", "Graduate", "Doctoral"]
-    industry = TextField('Industry') #TODO: This needs to be select list: [Construction, Education, Government, Technology]
-    profession = TextField('Profession')  #TODO: This needs to be select list: [Electrician, Teacher, Project Manager, Software Engineer]
+    city = SelectField('City', choices=cities_tuple)
+
+    highest_level_of_education = SelectField('Highest Level of Education',
+        choices=[
+        ('High School','High School'),
+        ('Undergraduate','Undergraduate'),
+        ('Graduate','Graduate'),
+        ('Doctoral','Doctoral')
+        ]
+    )
+
+    industry = SelectField('Industry',
+        choices=[
+        ('Construction','Construction'),
+        ('Education','Education'),
+        ('Government','Government'),
+        ('Technology','Technology')
+        ]
+    )
+
+    profession = SelectField('Profession',
+        choices=[
+        ('Electrician','Electrician'),
+        ('Teacher','Teacher'),
+        ('Project Manager','Project Manager'),
+        ('Software Engineer','Software Engineer')
+        ]
+    )
+
     skills = TextAreaField('Skills (Comma Separated)')
-    minimum_salary_requirement = TextField(
-        'Minimum Salary Requirement (Monthly)')  #TODO: This needs to be select list: [200-299, 300-399, 400-499, 500-599, 600-699, 700-799, 800-899, 900-999, 1000+]
+
+
+    minimum_salary_requirement = SelectField('Minimum Salary Requirement (Monthly)',
+        choices=[
+        ('200-299','200-299'),
+        ('300-399','300-399'),
+        ('400-499','400-499'),
+        ('500-599','500-599'),
+        ('600-699','600-699,'),
+        ('700-799','700-799'),
+        ('800-899','800-899'),
+        ('900-999','900-999'),
+        ('1000+','1000+')
+        ]
+    )
